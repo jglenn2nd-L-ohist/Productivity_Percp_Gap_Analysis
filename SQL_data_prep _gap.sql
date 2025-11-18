a0ff6ed193698ef8872885029e2f762866aa6384
jglenn2nd-L-ohist <j.glenn2nd@gmail.com>
2025-11-18 15:02:19 -0500

    Add files via upload

---
+++ SQL_data_prep _gap.sql
@@ -0,0 +1,16 @@
+-- ADD column to reveal productivity gap
+ALTER TABLE
+    `avian-destiny-473116-a6.soc_med_v_prod.productivity_index`
+ADD COLUMN
+    productivity_gap NUMERIC;
+
+-- POPULATE column productivity gap & UPDATE table
+UPDATE
+  `avian-destiny-473116-a6`.`soc_med_v_prod`.`productivity_index`
+SET
+  productivity_gap = CAST(perceived_productivity_score - actual_productivity_score AS NUMERIC)
+WHERE
+  perceived_productivity_score IS NOT NULL
+  AND actual_productivity_score IS NOT NULL;
+
+
